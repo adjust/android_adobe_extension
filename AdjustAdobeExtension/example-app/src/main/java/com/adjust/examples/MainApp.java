42eb96a4efa1da1f49cfa7b20a139677959265f1
Shashank Suhane <shashank@adjust.com>
2020-11-03 14:09:42 +0100

    Add callbacks in example code

--- AdjustAdobeExtension/example-app/src/main/java/com/adjust/examples/MainApp.java
+++ AdjustAdobeExtension/example-app/src/main/java/com/adjust/examples/MainApp.java
@@ -1,9 +1,22 @@
 package com.adjust.examples;
 
 import android.app.Application;
+import android.net.Uri;
+import android.util.Log;
 
 import com.adjust.adobeextension.AdjustAdobeExtension;
 import com.adjust.adobeextension.AdjustAdobeExtensionConfig;
+import com.adjust.sdk.AdjustAttribution;
+import com.adjust.sdk.AdjustEventFailure;
+import com.adjust.sdk.AdjustEventSuccess;
+import com.adjust.sdk.AdjustSessionFailure;
+import com.adjust.sdk.AdjustSessionSuccess;
+import com.adjust.sdk.OnAttributionChangedListener;
+import com.adjust.sdk.OnDeeplinkResponseListener;
+import com.adjust.sdk.OnEventTrackingFailedListener;
+import com.adjust.sdk.OnEventTrackingSucceededListener;
+import com.adjust.sdk.OnSessionTrackingFailedListener;
+import com.adjust.sdk.OnSessionTrackingSucceededListener;
 import com.adobe.marketing.mobile.AdobeCallback;
 import com.adobe.marketing.mobile.Identity;
 import com.adobe.marketing.mobile.InvalidInitException;
@@ -28,6 +41,7 @@ public class MainApp extends Application {
         MobileCore.setLogLevel(LoggingMode.VERBOSE);
 
         // register Adobe core extensions
+        // below are just sample extensions, not limited to these
         try {
             Identity.registerExtension();
             Signal.registerExtension();
@@ -39,6 +53,65 @@ public class MainApp extends Application {
         // register the Adjust extension
         AdjustAdobeExtensionConfig config =
                 new AdjustAdobeExtensionConfig(AdjustAdobeExtensionConfig.ENVIRONMENT_SANDBOX);
+
+        // Optional callbacks
+        // Set attribution delegate.
+        config.setOnAttributionChangedListener(new OnAttributionChangedListener() {
+            @Override
+            public void onAttributionChanged(AdjustAttribution attribution) {
+                Log.d("example", "Attribution callback called!");
+                Log.d("example", "Attribution: " + attribution.toString());
+            }
+        });
+
+        // Set event success tracking delegate.
+        config.setOnEventTrackingSucceededListener(new OnEventTrackingSucceededListener() {
+            @Override
+            public void onFinishedEventTrackingSucceeded(AdjustEventSuccess eventSuccessResponseData) {
+                Log.d("example", "Event success callback called!");
+                Log.d("example", "Event success data: " + eventSuccessResponseData.toString());
+            }
+        });
+
+        // Set event failure tracking delegate.
+        config.setOnEventTrackingFailedListener(new OnEventTrackingFailedListener() {
+            @Override
+            public void onFinishedEventTrackingFailed(AdjustEventFailure eventFailureResponseData) {
+                Log.d("example", "Event failure callback called!");
+                Log.d("example", "Event failure data: " + eventFailureResponseData.toString());
+            }
+        });
+
+        // Set session success tracking delegate.
+        config.setOnSessionTrackingSucceededListener(new OnSessionTrackingSucceededListener() {
+            @Override
+            public void onFinishedSessionTrackingSucceeded(AdjustSessionSuccess sessionSuccessResponseData) {
+                Log.d("example", "Session success callback called!");
+                Log.d("example", "Session success data: " + sessionSuccessResponseData.toString());
+            }
+        });
+
+        // Set session failure tracking delegate.
+        config.setOnSessionTrackingFailedListener(new OnSessionTrackingFailedListener() {
+            @Override
+            public void onFinishedSessionTrackingFailed(AdjustSessionFailure sessionFailureResponseData) {
+                Log.d("example", "Session failure callback called!");
+                Log.d("example", "Session failure data: " + sessionFailureResponseData.toString());
+            }
+        });
+
+        // Evaluate deferred deep link to be launched.
+        config.setOnDeeplinkResponseListener(new OnDeeplinkResponseListener() {
+            @Override
+            public boolean launchReceivedDeeplink(Uri deeplink) {
+                Log.d("example", "Deferred deep link callback called!");
+                Log.d("example", "Deep link URL: " + deeplink);
+
+                return true;
+            }
+        });
+
+        // register the Adjust SDK extension
         AdjustAdobeExtension.registerExtension(config);
 
         // once all the extensions are registered, start processing the events
